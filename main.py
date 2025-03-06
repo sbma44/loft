c412457ac42eb57406d7764e2de58cb83008eb1d
Tom Lee <thomas.j.lee@gmail.com>
2025-03-06 18:13:58 -0500

    add more guard rails for buffer overflow error condition

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import time
 import logging
 import threading
 import atexit
+import gc
 import random
 import colorsys
 import functools
@@ -88,34 +89,49 @@ class SerialInterface(object):
 class NoteListener(object):
     def __init__(self, silence, callback):
         # initialise pyaudio
-        self.p = pyaudio.PyAudio()
-        self.callback = callback
+        self.start_audio()
 
-        # open stream
-        self.buffer_size = 1024
-        pyaudio_format = pyaudio.paFloat32
-        n_channels = 1
-        samplerate = 44100
-        self.stream = self.p.open(format=pyaudio_format,
-                        channels=n_channels,
-                        rate=samplerate,
-                        input=True,
-                        frames_per_buffer=self.buffer_size)
+        self.callback = callback
 
         # setup pitch
         tolerance = 0.8
         win_s = 4096 # fft size
         hop_s = self.buffer_size # hop size
 
-        self.note_o = aubio.notes("default", win_s, hop_s, samplerate)
+        self.note_o = aubio.notes("default", win_s, hop_s, self.samplerate)
         self.note_o.set_silence(silence)
 
-        self.pitch_o = aubio.pitch("default", win_s, hop_s, samplerate)
+        self.pitch_o = aubio.pitch("default", win_s, hop_s, self.samplerate)
         self.pitch_o.set_unit("midi")
         self.pitch_o.set_tolerance(tolerance)
 
         self.thread = threading.Thread(target=self.listen)
         self.thread.start()
+
+    def start_audio(self):
+        gc.collect()
+        self.p = pyaudio.PyAudio()
+
+        self.buffer_size = 512 # 1024
+        self.pyaudio_format = pyaudio.paFloat32
+        self.n_channels = 1
+        self.samplerate = 44100
+        logging.info('starting stream...')
+        self.stream = self.p.open(format=self.pyaudio_format,
+            channels=self.n_channels,
+            rate=self.samplerate,
+            input=True,
+            frames_per_buffer=self.buffer_size)
+        while not self.stream.is_active():
+            time.sleep(0.1)
+
+    def stop_audio(self):
+        logging.debug('stopping stream...')
+        self.stream.stop_stream()
+        while not self.stream.is_stopped():
+            time.sleep(0.1)
+        self.stream.close()
+        self.p.terminate()
 
     def _midi_to_pitch(self, midi_number):
         pitch_names = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
@@ -137,14 +153,16 @@ class NoteListener(object):
 
                 if note[0] > 0:
                     self.callback(self._midi_to_pitch(note[0]), note, pitch, confidence)
-            except:
-                pass
+
+            except Exception as e:
+                logging.error(f"Error listening for notes: {e}")
+                logging.debug('restarting audio...')
+                self.stop_audio()
+                self.start_audio()
 
     def cleanup(self):
         logging.debug('done recording, cleaning up...')
-        self.stream.stop_stream()
-        self.stream.close()
-        self.p.terminate()
+        self.stop_audio()
 
 class Main(object):
     def __init__(self):
